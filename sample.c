4574f15d61d337c5f1d241d7d911114568eeb137
nikhil4969 <nikhilmahesh4969@gmail.com>
2017-03-20 16:18:29 +0530

    This is the Second Commit

--- sample.c
+++ sample.c
@@ -2,5 +2,8 @@
 void main()
 {
  printf("Hello World this is Nikhil on git ");
+ printf("This is the Second line in the Code ");
+ // This is a Line Comment 
+ /*This is a Block Line Comment */ 
  return;
 }
